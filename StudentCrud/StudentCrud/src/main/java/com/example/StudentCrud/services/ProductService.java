619f6fd08bb99ed288e1b79a5bd6e21044d64564
Shani Revach <shanirevach99@gmail.com>
2022-01-23 14:21:17 +0200

    Product service

---
+++ StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/services/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.StudentCrud.services;
+
+import com.example.StudentCrud.domain.Product;
+import com.example.StudentCrud.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> listAll(String keyword) {
+        if (keyword != null) {
+            return productRepository.search(keyword);
+        }
+        return productRepository.findAll();
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public void delete(long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product get(long id) {
+        return productRepository.findById(id).get();
+    }
+
+}
