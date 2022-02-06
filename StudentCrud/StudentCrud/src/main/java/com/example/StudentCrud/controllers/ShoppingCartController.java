20cd2a5ee2adb835e130352d07d3d4912c3d64be
Shani Revach <shanirevach99@gmail.com>
2022-02-06 15:51:29 +0200

    Fix ShoppingCartController

--- StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/controllers/ShoppingCartController.java
+++ StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/controllers/ShoppingCartController.java
@@ -4,6 +4,7 @@ import com.example.StudentCrud.domain.Item;
 import com.example.StudentCrud.domain.Product;
 import com.example.StudentCrud.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class ShoppingCartController {
 
     private final ProductService productService;
