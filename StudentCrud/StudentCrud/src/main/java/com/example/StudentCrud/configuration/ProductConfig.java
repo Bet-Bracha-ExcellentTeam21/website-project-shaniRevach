bb5284cf17784e92baec753af3ff9f02e4f3684f
Shani Revach <shanirevach99@gmail.com>
2022-02-09 15:10:20 +0200

    Fix bean

---
+++ StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/configuration/ProductConfig.java
@@ -0,0 +1,23 @@
+package com.example.StudentCrud.configuration;
+
+import com.example.StudentCrud.domain.Product;
+import com.example.StudentCrud.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductConfig {
+
+	@Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args -> {
+			Product opera = new Product("opera", "images/opera.html", 20.0);
+			productRepository.save(opera);
+			Product chocolatePie = new Product("coconut chocolate pie", "images/coconut chocolate pie.html", 15.0);
+			productRepository.save(chocolatePie);
+			Product chocolateEclair = new Product("chocolate eclair", "images/chocolate eclair.html", 13.0);
+			productRepository.save(chocolateEclair);
+		};
+	}
+}
