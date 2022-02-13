275f6415ef313045137dead250b18f8a3c14da1f
Shani Revach <shanirevach99@gmail.com>
2022-02-13 11:31:14 +0200

    Change pictures

--- StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/configuration/ProductConfig.java
+++ StudentCrud/StudentCrud/src/main/java/com/example/StudentCrud/configuration/ProductConfig.java
@@ -12,11 +12,11 @@ public class ProductConfig {
 	@Bean
     CommandLineRunner commandLineRunner(ProductRepository productRepository){
 		return args -> {
-			Product opera = new Product("opera", "assets/images/opera.html", 20.0);
+			Product opera = new Product("opera", "assets/images/opera.png", 20.0);
 			productRepository.save(opera);
-			Product chocolatePie = new Product("coconut chocolate pie", "assets/images/coconut chocolate pie.html", 15.0);
+			Product chocolatePie = new Product("coconut chocolate mousse", "assets/images/coconut chocolate mousse.png", 15.0);
 			productRepository.save(chocolatePie);
-			Product chocolateEclair = new Product("chocolate eclair", "assets/images/chocolate eclair.html", 13.0);
+			Product chocolateEclair = new Product("chocolate eclair", "assets/images/chocolate eclair.png", 13.0);
 			productRepository.save(chocolateEclair);
 		};
 	}
